dfe8228502bccce7f5263e1ee97d00763df1f618
Mariano <mariano_farace@hotmail.com>
2022-03-22 08:26:54 +0100

    Cleanup

--- helpers.js
+++ helpers.js
@@ -19,36 +19,44 @@ const filterItAcademyUsers = (userArray) => {
 //Si el userid existe en EDB (como login) pero no EN LDB(como userid), LO CREA. username es obligatorio, y va a ser el login de la base comun
 //Va a ser un filter usando la funcion findByUserName de LDB
 const upsert = (externalDBUsers, userRepository) => {
+  let createdCount = 0;
+  let updatedCount = 0;
+
   externalDBUsers.forEach((eUser) => {
     let wasFound = userRepository.findByUserName(eUser.login);
     if (wasFound == undefined) {
-      console.log("paso por el crear nuevo!");
       userRepository.addUser({
         userName: eUser.login,
         externalId: eUser.id.toString(),
         picture: eUser.avatar_url,
       });
+      createdCount++;
     } else {
-      console.log("habia uno repetido");
       userRepository.updateUser({
         userName: eUser.login,
         externalId: eUser.id.toString(),
         picture: eUser.avatar_url,
       });
+      updatedCount++;
     }
   });
+
+  return { created: createdCount, updated: updatedCount };
 };
 
 //para borrar los usuario de la base de datos es exactamente al revez
 
 const deleteSync = (localDBUsers, externalDBUsers, userRepository) => {
+  let deletedCount = 0;
   let toBeDeleted = localDBUsers.filter(
     (localUser) =>
       !externalDBUsers.map((eUser) => eUser.login).includes(localUser.userName)
   );
-  toBeDeleted.forEach((user) =>
-    userRepository.deleteUserByUserName(user.userName)
-  );
+  toBeDeleted.forEach((user) => {
+    userRepository.deleteUserByUserName(user.userName);
+    deletedCount++;
+  });
+  return { deleted: deletedCount };
 };
 
 //el update te lo debo:: compara los objetos por unique key (username/login), si isEqual devuelve false, hace update
--- routes/syncUsers.js
+++ routes/syncUsers.js
@@ -13,15 +13,22 @@ const syncUsers = (userRepository) => {
     const totalExternalDBUsers = data.items;
     //External DB users
     const externalDBUsers = filterItAcademyUsers(totalExternalDBUsers);
-    console.log("externalDBUsers lenght", externalDBUsers.length);
     //Internal DB users
     const localDBUsers = userRepository.findAllUsers();
 
     //Delete users that exist on localDB but not externalDB
-    deleteSync(localDBUsers, externalDBUsers, userRepository);
-    upsert(externalDBUsers, userRepository);
+    const deleteResult = deleteSync(
+      localDBUsers,
+      externalDBUsers,
+      userRepository
+    );
+    const upsertResult = upsert(externalDBUsers, userRepository);
 
-    res.send("Synchronization succeeded");
+    res.json({
+      message: "Synchronization succeeded",
+      upsertResult,
+      deleteResult,
+    });
   };
 };
 
